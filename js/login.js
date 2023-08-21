1980e330ebc78310a7fa44158786e33d5d1270f9
Heltta <lucashelgueraz@gmail.com>
2023-08-21 13:21:22 -0300

    Actualizar pages con el repositorio grupal

---
+++ js/login.js
@@ -0,0 +1,46 @@
+// Function del Login:
+
+function ingresar(){
+
+  let user= document.getElementById('user').value;
+
+  let pass= document.getElementById('pass').value;
+
+  if(user.length>0 && pass.length>0){
+    setUserIsLogged(true)
+    window.location.href="index.html";
+  }
+}
+
+// EventListener para detectar el click en login.html y redirigir a index.html
+
+document.addEventListener('DOMContentLoaded', function(){
+  if (userIsLogged()) {
+    window.location.href = "index.html";
+  }
+  document.getElementById('login').addEventListener('click', function(){
+    ingresar();
+  })
+
+})
+
+/**
+ * Obtener estado de logged del usuario
+ *
+ * Obtiene del localStorage el valor de la data de key 'userIsLogged' en el localStorage
+ */
+function userIsLogged() {
+  return localStorage.getItem('userIsLogged') === "true";
+}
+
+/**
+ * Cambiar estado de logged del usuario
+ *
+ * Cambia el valor de la data de key 'userIsLogged' en el localStorage
+ * a true o false acorde a lo ingresado como parametro.
+ */
+function setUserIsLogged(isLogged) {
+  // Doble negación para evita asignar algo que no sea Bool
+  localStorage.setItem('userIsLogged', !!isLogged);
+}
+
---
+++ js/products.js
@@ -0,0 +1,101 @@
+/**
+ * Obtener autos
+ *
+ * Le pide a la api el listado de productos de la categoría autos (id 101).
+ * Retorna una promesa que al cumplirse devuelve un objeto del tipo Category
+ */
+async function getCars() {
+  const idCategoryCars = 101; // id de la categoría autos
+  try {
+    // Pide a la API la lista de productos en la categoría espera a que
+    // se resuelva la promesa y guarda la respuesta "cruda" en una constante
+    const respuestaAPI = await fetch(
+      `https://japceibal.github.io/emercado-api/cats_products/${idCategoryCars}.json`
+    );
+    // Parsea la respuesta y la retorna
+    // Nota: En realidad retorna una promesa (usar await o .then())
+    return respuestaAPI.json();
+  } catch (error) {
+    alert(error);
+  }
+}
+
+/** Generar elemento de item
+ *
+ * Recibe un objeto de tipo Product (como un Auto).
+ * Retorna un elemento del DOM con las caracteristicas de un auto.
+ * Ejemplo: que retorne un elem tal que elem.innerHTML == <div>Marca: Wolkswagen Modelo: Gol<div>.
+ */
+function generarElementoDeProduct(product) {
+  let htmlContentToAppend = `<li class="container">
+    <section class="row d-flex w-100 justify-content-between">
+      <div class="col">${product.name}</div>
+      <div class="col">${product.currency}${product.cost}</div>
+    </section>
+    <section class="row">
+      <div class="col">
+        <div>${product.description}</div>
+        <div>Cantidad vendidos: ${product.soldCount}</div>
+      </div>
+      <div class="col"><img class="col-4" src=${product.image}></div>
+    </section>
+  </li>`; /*aca va el contenido html que se generara para mostrar el producto*/
+
+
+  let productDOM = document.createElement('div'); //aca se crea el elemento del dom que incluira el contenido html
+  productDOM.id = product.id; // se agrega el id al div
+  productDOM.innerHTML = htmlContentToAppend; //aca se actualiza el contenido del elemento con el contenido de htmlContentToAppends
+  return productDOM;
+}
+
+/**
+ * Genera un elemento del DOM con una lista de elementos con datos
+ * de los Productos a partir de un arreglo de Productos
+ *
+ * Lista en pantalla una lista de items
+ */
+function generarElementoListaItems(listaDeItems) {
+  const listaProductos = document.createElement('ol');
+  for (let i = 0; i < listaDeItems.length; i++)
+    listaProductos.appendChild(generarElementoDeProduct(listaDeItems[i]));
+  listaProductos.style= "list-style: none";
+  return listaProductos;
+}
+
+/**
+ * Eliminar alerta de "En desarrollo"
+ *
+ * Busca y elimina la alerta en rojo de "Funcionalidad en desarrollo".
+ */
+function eliminarEnDesarrollo() {
+  let alerta = document.getElementsByClassName(
+    'alert alert-danger text-center'
+  );
+  alerta[0].remove();
+}
+
+function mostrarListaItems(listaDeItems) {
+  const listaDeProductos = generarElementoListaItems(listaDeItems);
+  const contenedorDeLista = document.getElementById('contenedor-productos');
+  contenedorDeLista.appendChild(listaDeProductos);
+}
+
+/**
+ * Muestra, dentro de un elemento contenedor del HTML, todos
+ * los objetos Product de la Category cars (id 101)
+ *
+ */
+async function printAllCars() {
+  try {
+    const categoriaAutos = await getCars();
+    mostrarListaItems(categoriaAutos.products);
+  } catch (error) {
+    alert(error);
+  }
+}
+
+// Llamados a las funciones luego de que el DOM se cargue
+document.addEventListener('DOMContentLoaded', () => {
+  eliminarEnDesarrollo();
+  printAllCars();
+});
